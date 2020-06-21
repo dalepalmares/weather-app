edbd98fd69dfb1e4d3ebb564d0f24b47c2c86905
yelaD <dale@internold.com>
2020-06-21 16:46:54 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const api = {
+  key: "8f546035ead3ad9b1052f01bb1c7ad88",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener("keypress", setQuery);
+
+function setQuery(e) {
+  if (e.keyCode === 13) {
+    getResults(searchBox.value);
+  }
+}
+
+function getResults(q) {
+  fetch(`${api.base}weather?q=${q}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuider(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )}°c`;
+}
+
+function dateBuider(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
